408551237e9bd7dbb96e52f5c0dc11fa8505203e
David Smith <codeactual@gmail.com>
2011-09-19 23:03:28 +0000

    initial import

---
+++ src/CurlyQueue.php
@@ -0,0 +1,298 @@
+<?php
+/**
+ * CurlyQueue class
+ *
+ * @package CurlyQueue
+ */
+
+use \Flow\Interruptable;
+
+require_once __DIR__ . '/../vendor/flow/src/Flow/Interruptable.php';
+
+/**
+ * Mutli-handle, asynchronous, chunked cURL queue with per-request callbacks
+ * and context variables.
+ */
+class CurlyQueue implements Interruptable
+{
+  /**
+   * @var array
+   *
+   *      array(array('url' => 'url1', 'requestObj' => 'requestObj1'),
+   *            array('url' => 'url2', 'requestObj' => 'requestObj2'),
+   *            ...);
+   */
+  protected $queue;
+
+  /**
+   * @var array Free-form variables sent to callback to associate
+   *            async. responses with origin requests.
+   *            Indexed by cURL handle. (Can't rely on URL keys since
+   *            curl_getinfo() returns the last effective, not initial, URL).
+   */
+  protected $requestObjs;
+
+  /**
+   * @var callback Post-response callback.
+   */
+  protected $responseCallback;
+
+  /**
+   * @var callback Post-error callback.
+   */
+  protected $errorCallback;
+
+  /**
+   * @var callback Post-queue-completion callback.
+   */
+  protected $endCallback;
+
+  /**
+   * @var array cURL options.
+   */
+  protected $curlOpts;
+
+  /**
+   * @var callable See \Flow\Interruptable interface.
+   */
+  protected $interruptCheck;
+
+  /**
+   * @var resource $mh Handle from curl_multi_init().
+   */
+  protected $mh;
+
+  /**
+   * Inject dependencies.
+   *
+   * @param array $curlOpts See $this->curlOpts.
+   * @return void
+   */
+  public function __construct($curlOpts)
+  {
+    $this->curlOpts = $curlOpts;
+  }
+
+  /**
+   * For Interruptable.
+   *
+   * {@inheritdoc}
+   */
+  public function setInterruptCheck($check)
+  {
+    $this->interruptCheck = $check;
+  }
+
+  /**
+   * For Interruptable.
+   *
+   * @inheritdoc
+   */
+  public function isInterrupted()
+  {
+    if (call_user_func($this->interruptCheck)) {
+      $this->endExec();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Set the post-response callback.
+   *
+   * @param callback $callback
+   * @return void
+   */
+  public function setResponseCallback($callback)
+  {
+    $this->responseCallback = $callback;
+  }
+
+  /**
+   * Set the post-error callback.
+   *
+   * @param callback $callback
+   * @return void
+   */
+  public function setErrorCallback($callback)
+  {
+    $this->errorCallback = $callback;
+  }
+
+  /**
+   * Set the post-queue callback.
+   *
+   * @param callback $callback
+   * @return void
+   */
+  public function setEndCallback($callback)
+  {
+    $this->endCallback = $callback;
+  }
+
+  /**
+   * Add a URL to the queue.
+   *
+   * @param string $url
+   * @param mixed $requestObj Request identifer(s). See $queue.
+   * @return void
+   */
+  public function add($url, $requestObj)
+  {
+    $this->queue[] = array('url' => $url, 'requestObj' => $requestObj);
+  }
+
+  /**
+   * Read a URL from the queue and add the normal cURL handle
+   * to a multi-session handle
+   *
+   * @param int $queuePos Position in $queue.
+   * @return int Returns 0 on success, or one of the CURLM_XXX errors code.
+   */
+  protected function multiAddHandle($queuePos)
+  {
+    $ch = curl_init();
+
+    // Link handle and request ID data for reassociation in the callback.
+    $requestKey = (string) $ch;
+    $this->requestObjs[$requestKey] = $this->queue[$queuePos]['requestObj'];
+
+    // Apply default cURL options.
+    $options = $this->curlOpts;
+    $options[CURLOPT_URL] = $this->queue[$queuePos]['url'];
+    $options[CURLOPT_RETURNTRANSFER] = true;
+    curl_setopt_array($ch, $options);
+
+    return curl_multi_add_handle($this->mh, $ch);
+  }
+
+  /**
+   * Fetch queue in asynchronous batches.
+   *
+   *  -   Uses Josh Fraser's rolling queue approach (see link).
+   *  -   Callback and cURL options persist afterward.
+   *
+   * @link http://onlineaspect.com/2009/01/26/how-to-use-curl_multi-without-blocking/
+   * @param int $maxSimul Max cURL handles executed simultaneously.
+   * @return boolean True if all handles executed successfully.
+   */
+  public function exec($maxSimul = 4)
+  {
+    $this->mh = curl_multi_init();
+    $queueRemain = count($this->queue);
+    $queuePos = 0;
+
+    // Modulate async. batch size based on limit and queue size.
+    if ($maxSimul > $queueRemain) {
+      $batchSize = min($maxSimul, $queueRemain);
+    } else {
+      $batchSize = min($maxSimul, $queueRemain);
+    }
+
+    // Seed initial batch.
+    for ($b = 0; $b < $batchSize; $b++) {
+      $this->multiAddHandle($queuePos);
+
+      $queuePos++;
+      $queueRemain--;
+    }
+
+    $startTime = time();
+    $running = null;
+
+    do {
+      if ($this->isInterrupted()) {
+        return;
+      }
+
+      // Still working on one of the handles.
+      do {
+        // @codeCoverageIgnoreStart
+        if ($this->isInterrupted()) {
+          return;
+        }
+        // @codeCoverageIgnoreEnd
+
+        $execrun = curl_multi_exec($this->mh, $running);
+      } while ($execrun == CURLM_CALL_MULTI_PERFORM);
+
+      // Overall session error.
+      // @codeCoverageIgnoreStart
+      if($execrun != CURLM_OK) {
+        $logMsg = 'cURL multi-exec failed with code: ' . $execrun;
+        break;
+      }
+      // @codeCoverageIgnoreEnd
+
+      // One response is ready.
+      while($done = curl_multi_info_read($this->mh)) {
+        // @codeCoverageIgnoreStart
+        if ($this->isInterrupted()) {
+          return;
+        }
+        // @codeCoverageIgnoreEnd
+
+        $info = curl_getinfo($done['handle']);
+        $requestKey = (string) $done['handle'];
+
+        if (200 == $info['http_code']) {
+          if ($this->responseCallback) {
+            call_user_func(
+              $this->responseCallback,
+              $done['handle'],
+              curl_multi_getcontent($done['handle']),
+              $this->requestObjs[$requestKey]
+            );
+          }
+
+          // Add one more request from the queue.
+          if ($queueRemain) {
+            $this->multiAddHandle($queuePos);
+
+            $queuePos++;
+            $queueRemain--;
+            $running = true;
+          }
+        } else {
+          if ($this->errorCallback) {
+            call_user_func(
+              $this->errorCallback,
+              $done['handle'],
+              $this->requestObjs[$requestKey]
+            );
+          }
+
+          if ($queueRemain) {
+            // Move on to another element
+            $this->multiAddHandle($queuePos);
+
+            $queuePos++;
+            $queueRemain--;
+            $running = true;
+          }
+        }
+
+        curl_multi_remove_handle($this->mh, $done['handle']);
+      }
+    } while ($running);
+
+    $this->endExec();
+  }
+
+  /**
+   * Clean up after exec().
+   *
+   * @return void
+   */
+  protected function endExec()
+  {
+    curl_multi_close($this->mh);
+
+    $this->queue = array();
+    $this->requestObjs = array();
+
+    if ($this->endCallback) {
+      call_user_func($this->endCallback);
+    }
+  }
+}
---
+++ test/CurlyQueueTest.php
@@ -0,0 +1,213 @@
+<?php
+
+require_once __DIR__. '/../src/CurlyQueue.php';
+require_once __DIR__. '/../vendor/flow/src/Flow/Flow.php';
+
+use \CurlyQueue;
+use \Flow\Flow;
+
+class CurlyQueueTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp()
+  {
+    parent::setUp();
+
+    $this->queueConfig = array(
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_CONNECTTIMEOUT => 2,
+      CURLOPT_MAXREDIRS => 5,
+      CURLOPT_FOLLOWLOCATION => 1
+    );
+
+    $this->queue = new CurlyQueue($this->queueConfig);
+
+    Flow::setMaxRuntime($this->queue, 10);
+
+    $this->extUrlConfig = array(
+      'http://www.google.com/' => uniqid(),
+      'http://twitter.com/' => uniqid(),
+      'http://www.youtube.com/' => uniqid(),
+      'http://www.hulu.com/' => uniqid()
+    );
+  }
+
+  /**
+   * Merged testing of both events to verify their completion order.
+   *
+   * @group runsResponseAndEndCallback
+   * @test
+   */
+  public function runsResponseAndEndCallback()
+  {
+    $responses = array();
+    $that = $this;
+
+    $this->queue->setResponseCallback(function ($ch, $content, $requestObj) use (&$responses, $that) {
+      $info = curl_getinfo($ch);
+      $that->assertSame(200, $info['http_code']);
+      $that->assertGreaterThanOrEqual(9000, $info['size_download']);
+      $responses[$info['url']] = $requestObj;
+    });
+
+    $this->queue->setEndCallback(function () use ($that, &$responses) {
+      // Called after all response events
+      $that->assertSame(count($that->extUrlConfig), count($responses), var_export($responses, true));
+    });
+
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+    $this->queue->exec();
+
+    $this->assertSame(count($this->extUrlConfig), count($responses));
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->assertSame($requestObj, $responses[$url]);
+    }
+  }
+
+  /**
+   * @group runsErrorCallbackForNon200
+   * @test
+   */
+  public function runsErrorCallbackForNon200()
+  {
+    $responses = array();
+
+    $that = $this;
+    $this->queue->setErrorCallback(function ($ch, $requestObj) use (&$responses, $that) {
+      $info = curl_getinfo($ch);
+      $that->assertSame(404, $info['http_code']);
+      $responses[$info['url']] = $requestObj;
+    });
+
+    $urlConfig = array(
+      'http://www.amazon.com/badUrl-0' => uniqid(),
+      'http://www.yahoo.com/badUrl-1' => uniqid(),
+      'http://www.google.com/badUrl-2' => uniqid(),
+      'https://twitter.com/badUrl-3' => uniqid()
+    );
+    foreach ($urlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+    $this->queue->exec();
+
+    $this->assertSame(count($urlConfig), count($responses), var_export(array_keys($responses), true));
+    foreach ($urlConfig as $url => $requestObj) {
+      $this->assertSame($requestObj, $responses[$url]);
+    }
+  }
+
+  /**
+   * @group runsErrorCallbackForLookups
+   * @test
+   */
+  public function runsErrorCallbackForLookups()
+  {
+    $responses = array();
+
+    $that = $this;
+    $this->queue->setErrorCallback(function ($ch, $requestObj) use (&$responses, $that) {
+      $info = curl_getinfo($ch);
+      $that->assertSame(0, $info['http_code']);
+      $that->assertSame(0, $info['request_size']);
+      $responses[$info['url']] = $requestObj;
+    });
+
+    // Port which is most likely unused
+    $urlConfig = array(
+      'http://localhost:8070/badUrl-0' => uniqid(),
+      'http://localhost:8070/badUrl-1' => uniqid(),
+      'http://localhost:8070/badUrl-2' => uniqid(),
+      'http://localhost:8070/badUrl-3' => uniqid(),
+      'http://localhost:8070/badUrl-4' => uniqid()
+    );
+    foreach ($urlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+    $this->queue->exec();
+
+    $this->assertSame(count($urlConfig), count($responses));
+    foreach ($urlConfig as $url => $requestObj) {
+      $this->assertSame($requestObj, $responses[$url]);
+    }
+  }
+
+  /**
+   * @group isInterruptable
+   * @test
+   */
+  public function isInterruptable()
+  {
+    $responses = array();
+
+    $this->queue->setResponseCallback(function ($ch, $content, $requestObj) use (&$responses) {
+      $info = curl_getinfo($ch);
+      $responses[$info['url']] = $requestObj;
+    });
+
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+
+    // exec() should stop before any work
+    Flow::setMaxRuntime($this->queue, 0);
+    $this->queue->exec();
+
+    $this->assertSame(0, count($responses));
+  }
+
+  /**
+   * @group handlesBatchLargerThanQueue
+   * @test
+   */
+  public function handlesBatchLargerThanQueue()
+  {
+    $responses = array();
+    $this->queue->setResponseCallback(function ($ch, $content, $requestObj) use (&$responses) {
+      $info = curl_getinfo($ch);
+      $responses[$info['url']] = $requestObj;
+    });
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+
+    $queueSize = count($this->extUrlConfig);
+
+    $this->queue->exec($queueSize * 2);
+    $this->assertSame($queueSize, count($responses));
+  }
+
+  /**
+   * @group handlesBatchSmallerThanQueue
+   * @test
+   */
+  public function handlesBatchSmallerThanQueue()
+  {
+    $responses = array();
+    $that = $this;
+
+    $this->queue->setResponseCallback(function ($ch, $content, $requestObj) use (&$responses, $that) {
+      $info = curl_getinfo($ch);
+      $that->assertSame(200, $info['http_code']);
+      $that->assertGreaterThanOrEqual(9000, $info['size_download']);
+      $responses[$info['url']] = $requestObj;
+    });
+
+    $queueSize = count($this->extUrlConfig);
+
+    $this->queue->setEndCallback(function () use ($queueSize, $that, &$responses) {
+      // Called after all response events
+      $that->assertSame($queueSize, count($responses), var_export($responses, true));
+    });
+
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->queue->add($url, $requestObj);
+    }
+    $this->queue->exec($queueSize / 2);
+
+    $this->assertSame($queueSize, count($responses));
+    foreach ($this->extUrlConfig as $url => $requestObj) {
+      $this->assertSame($requestObj, $responses[$url]);
+    }
+  }
+}
